d91dedad34530e9cee2991a9ace26a6a276d0069
Sarah Penney <sarahpenneyuk@gmail.com>
2018-02-12 15:52:35 +0000

    updated: inject vue data

--- js/app.js
+++ js/app.js
@@ -1,38 +1,38 @@
 
-var basic = new Vue ({
-  el: '#basic',
-  data: {
-    message: 'Hello Vue, I was made with an ID'
-  }
-});
-
-var classElement = new Vue ({
-  el: '.class-element',
-  data: {
-    message: 'Hello Vue, I was made with a class'
-  }
-});
+// var basic = new Vue ({
+//   el: '#basic',
+//   data: {
+//     message: 'Hello Vue, I was made with an ID'
+//   }
+// });
+//
+// var classElement = new Vue ({
+//   el: '.class-element',
+//   data: {
+//     message: 'Hello Vue, I was made with a class'
+//   }
+// });
 
 var me = new Vue ({
   el: '.me',
   data: {
     firstName: 'Sarah',
     lastName: 'Penney',
-    town: 'London',
+    town: 'Manchester',
     age: 29
   }
 });
 
 
-var helloButton = new Vue ({
-    el: '#hello',
-    data: {},
-    methods: {
-        sayHello: function () {
-            alert("Hello");
-        }
-    }
-});
+// var helloButton = new Vue ({
+//     el: '#hello',
+//     data: {},
+//     methods: {
+//         sayHello: function () {
+//             alert("Hello");
+//         }
+//     }
+// });
 
 var shoppingList = new Vue ({
     el: '#shoppingList',
@@ -53,10 +53,10 @@ var products = new Vue ({
     el: '#products',
     data: {
         menu: [
-          {text: "Melody", price: "10p", imageSrc: "images/ada.jpg"},
-          {text: "Keeper", price: "30p", imageSrc: "/images/face.jpg"},
-          {text: "Me", price: "20p", imageSrc: "/images/IMG_1188.jpg"},
-          {text: "Jason", price: "40p", imageSrc: "/images/static1.squarespace.png"}
+          {text: "Melody:", price: "£3.10p", imageSrc: "images/ada.jpg"},
+          {text: "Keeper:", price: "£2.30p", imageSrc: "/images/face.jpg"},
+          {text: "Me:", price: "£1.20p", imageSrc: "/images/IMG_1188.jpg"},
+          {text: "Jason:", price: "£2.40p", imageSrc: "/images/static1.squarespace.png"}
         ]
     }
 });
